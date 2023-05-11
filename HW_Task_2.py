e3a7da94b687616c0a0ff7b5f86131d0ff8fc446
Green-Mad <beliberda111@gmail.com>
2023-05-11 14:27:19 +0300

    #merger of companies

---
+++ HW_Task_2.py
@@ -0,0 +1,9 @@
+#merger of companies
+#Eleks_employees
+#Toshiba_employees
+
+Eleks_employees = ['Freya', 'Jasmine', 'Mistress', 'Ariel']
+Toshiba_employees = ['Zoreslav', 'Moses', 'Lord', 'Marilouise']
+
+Toshiba_employees.extend(Eleks_employees)
+print(Toshiba_employees)
