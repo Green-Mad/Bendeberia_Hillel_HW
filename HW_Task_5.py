a9b009902bb2b73ed32d5f795967309ed58f782c
Green-Mad <beliberda111@gmail.com>
2023-05-12 15:47:08 +0300

    commit fix

--- HW_Task_5.py
+++ HW_Task_5.py
@@ -1,4 +1,4 @@
-VIP = {'Tony Stark', 'Bruse Wayne', 'Lex Luthor'}
-Common = ('Barry Alen', 'Peter Parker', 'Antman', None, None)
-print(VIP)
-print(Common)
+vip = {'Tony Stark', 'Bruse Wayne', 'Lex Luthor'}
+common = ['Barry Alen', 'Peter Parker', 'Antman', None, None]
+print(vip)
+print(common)
