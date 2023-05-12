c7ae2a0bba3c321dce0784d4945bfba018a3ebaa
Green-Mad <beliberda111@gmail.com>
2023-05-12 15:55:34 +0300

    commit fix

--- HW_Task_6.py
+++ HW_Task_6.py
@@ -1,6 +1,4 @@
 non_unik_name = ['John', 'Marta', 'Alex', 'John', 'Sarah', 'Alex', 'Kate', 'Marta']
-unique_names_dict = {}
-for key in non_unik_name:
-    unique_names_dict[key] = None
-unique_names = list(unique_names_dict.keys())
+unique_names = list(dict.fromkeys(non_unik_name))
+
 print(unique_names)
