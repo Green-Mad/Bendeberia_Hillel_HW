135f8d100e60b3ba2c4405291b11fdbe355fd642
Green-Mad <beliberda111@gmail.com>
2023-05-12 11:52:22 +0300

    fix tuple

--- HW_Task_5.py
+++ HW_Task_5.py
@@ -1,4 +1,4 @@
 VIP = {'Tony Stark', 'Bruse Wayne', 'Lex Luthor'}
-Common = ('Barry Alen', 'Peter Parker', 'Antman', )
+Common = ('Barry Alen', 'Peter Parker', 'Antman', None, None)
 print(VIP)
 print(Common)
