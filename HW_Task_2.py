e46d4c2fb06b7d928268734b1207eba2aec56de4
Green-Mad <beliberda111@gmail.com>
2023-05-14 13:50:31 +0300

    task2 task5

--- HW_Task_2.py
+++ HW_Task_2.py
@@ -5,8 +5,8 @@
 eleks_employees = ['Freya', 'Jasmine', 'Mistress', 'Ariel', 'Kyrylo']
 toshiba_employees = ['Zoreslav', 'Moses', 'Kyrylo', 'Lord', 'Marilouise']
 
-toshiba_employees += eleks_employees
-eleks_employees = []
+toshiba_employees.extend(eleks_employees)
+eleks_employees.clear()
 
 print("Toshiba employees:", toshiba_employees)
 print("Eleks employees:", eleks_employees)
