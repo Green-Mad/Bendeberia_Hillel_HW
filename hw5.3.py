6576933b7de3b97cfc52f526bc8c9b8f83dcb1c4
Green-Mad <beliberda111@gmail.com>
2023-05-31 12:32:25 +0300

    fixed task 3

--- hw5.3.py
+++ hw5.3.py
@@ -4,14 +4,11 @@ file_path = './text.txt'
 letter_counts = {}
 
 with open(file_path, 'r') as file:
-    for line in file:
-        for char in line:
-            if char.isalpha():
-                char = char.lower()
-                if char in letter_counts:
-                    letter_counts[char] += 1
-                else:
-                    letter_counts[char] = 1
+    text = file.read().lower()
+
+for char in text:
+    if char.isalpha():
+        letter_counts[char] = letter_counts.get(char, 0) + 1
 
 for letter, count in letter_counts.items():
     print(f"{letter}: {count}")
