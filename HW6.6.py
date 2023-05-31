8f349d1438f4478f674056a854e7a6b6deefc526
Green-Mad <beliberda111@gmail.com>
2023-05-31 16:27:32 +0300

    finish task 6

--- HW6.6.py
+++ HW6.6.py
@@ -2,10 +2,11 @@ def calculate_sum(*args):
     return sum(args)
 
 result = calculate_sum()
-# print(result)
-
-import random
-
-args = [random.randint(1, 10) for _ in range(5)]
-result = calculate_sum(*args)
 print(result)
+
+# Перевірка
+# import random
+#
+# args = [random.randint(1, 10) for _ in range(5)]
+# result = calculate_sum(*args)
+# print(result)
