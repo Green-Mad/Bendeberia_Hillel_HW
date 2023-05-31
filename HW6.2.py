6277032025334354bc28e6c454251b96c0506278
Green-Mad <beliberda111@gmail.com>
2023-05-31 12:52:54 +0300

    finish task 2

--- HW6.2.py
+++ HW6.2.py
@@ -1,1 +1,14 @@
-HW6.1.py
+import math
+import random
+def square(side):
+    perimeter = 4 * side
+    area = side ** 2
+    diagonal = math.sqrt(2) * side
+    return perimeter, area, diagonal
+
+side = random.randint(1, 1000)
+perimeter, area, diagonal = square(side)
+
+print("Perimeter:", perimeter)
+print("Area:", area)
+print("Diagonal:", diagonal)
