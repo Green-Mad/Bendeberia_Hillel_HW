17ab10173e17ce9ce9ca3f92fccbe49051446021
Green-Mad <beliberda111@gmail.com>
2023-06-05 16:31:00 +0300

    finish task2

--- hw7.2.py
+++ hw7.2.py
@@ -1,10 +1,14 @@
 def my_filter(function, iterable):
     filtered_list = [item for item in iterable if function(item)]
     return filtered_list
-def is_even(num):
-    return num % 2 == 0
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-filtered_numbers = my_filter(is_even, numbers)
+# для прикладу
 
-print(filtered_numbers)
+# def is_even(num):
+#     return num % 2 == 0
+#
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# filtered_numbers = my_filter(is_even, numbers)
+# print(filtered_numbers)
+
+print(my_filter)
