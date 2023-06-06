087d9cc6d56f464a2cd77480c1fa2af799a30e0d
Green-Mad <beliberda111@gmail.com>
2023-06-06 13:39:45 +0300

    finish task 5

---
+++ hw7.5.py
@@ -0,0 +1,9 @@
+def my_all(iterable):
+    for element in iterable:
+        if not element:
+            return False
+    return True
+
+numbers = [2, 4, 6, -15, 4.3]
+res = my_all(number > 0 for number in numbers)
+print('Numbers are float:', res)
