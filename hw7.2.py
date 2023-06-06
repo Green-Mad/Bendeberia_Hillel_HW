446bf54ac44f95d6f811658867f4c0c488b4c038
Green-Mad <beliberda111@gmail.com>
2023-06-06 13:53:13 +0300

    finish task 2

--- hw7.2.py
+++ hw7.2.py
@@ -1,14 +1,23 @@
-def my_filter(function, iterable):
-    filtered_list = [item for item in iterable if function(item)]
-    return filtered_list
+from typing import Callable, Iterable, List, Any
 
-# для прикладу
+def my_filter(callback: Callable[[Any], bool], sequence: Iterable) -> List[Any]:
+    filtered_elements = []
+    for element in sequence:
+        if callback(element):
+            filtered_elements.append(element)
+    return filtered_elements
 
-# def is_even(num):
-#     return num % 2 == 0
-#
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# filtered_numbers = my_filter(is_even, numbers)
-# print(filtered_numbers)
+def is_even(num: int) -> bool:
+    return num % 2 == 0
 
-print(my_filter)
+def is_long(word: str) -> bool:
+    return len(word) > 5
+
+numbers = [1, 2, 3, 4, 5]
+words = ["apple", "banana", "cherry", "orange"]
+
+filtered_numbers = my_filter(is_even, numbers)
+print("Filtered even numbers:", filtered_numbers)
+
+filtered_words = my_filter(is_long, words)
+print("Filtered long words:", filtered_words)
