8191a1792c3ad85ad94c95eb9ffbb34f8bdf07b1
Green-Mad <beliberda111@gmail.com>
2023-06-08 14:03:18 +0300

    fixed

--- hw8.4.py
+++ hw8.4.py
@@ -1,4 +1,4 @@
 # Find all of the numbers from 1-1000 that have a 3 in them
 
-numbers_with_3 = [number for number in range(1, 1001) if '3' in str(number)]
-print(numbers_with_3)
+numbers_with_3 = (number for number in range(1, 1001) if '3' in str(number))
+print(list(numbers_with_3))
